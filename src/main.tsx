99e317663ca2d578c2cda0c9de4612ed8323fa48
simon <starlxa@icloud.com>
2026-03-13 13:39:49 +0800

    fix: add @ant-design/v5-patch-for-react-19 to suppress React 19 compat warning

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import '@ant-design/v5-patch-for-react-19';
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { ConfigProvider, App as AntApp, theme } from 'antd';
